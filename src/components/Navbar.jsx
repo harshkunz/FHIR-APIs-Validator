7bc4ec058d48c554091fb96e829137dbc30f34e8
harshkunz <harshkunz0@gmial.com>
2025-07-26 02:15:57 +0530

    styling navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,34 +1,69 @@
-import { useState, useContext } from 'react';
+import { useState, useContext, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import { AlignJustify, AudioWaveform, History, X } from 'lucide-react';
+import { useFirebase } from '../context/Firebase';
+
 
 const Navbar = () => {
-    const isLoggedIn = false;
+    const [show, setShow] = useState(false);
+    const isLoggedIn = true;
+
+    const firebase = useFirebase();
+
+    const handleClick = () => {
+        if(show === true) setShow(false)
+        else setShow(true)
+    }
 
   return (
-    <nav className='fixed top-0 w-full z-99 bg-red-500 flex justify-between items-center h-14 p-2 text-white '>
-        <div>
-            <h3>logo</h3>
-        </div>
+    <>
+        <nav className='fixed top-0 w-full z-91 bg-black flex justify-between items-center h-14 p-2 text-white '>
+            <div className='m-2'>
+                <AudioWaveform size={30}/>
+            </div>
 
-        <div className=''>
-            {isLoggedIn ? (
-                <>
-                <Link>
-                    <span>login</span>
-                </Link>
-                </>
-            ) : (
-                <>
-                <Link>
-                    <span>history</span>
-                </Link>
-                <button>
-                    <span>logout</span>
-                </button>
-                </>
-            )}
-        </div>
-    </nav>
+            <div className=''>
+                {isLoggedIn ? (
+                    <>
+                    <div className='m-2 sm:hidden'>
+                        <AlignJustify className='cursor-pointer' onClick={handleClick}/>
+                    </div>
+                    <div className='p-2 text-[14px] hidden sm:block'>
+                        <Link className='mr-4 cursor-pointer'>
+                            <span>History</span>
+                        </Link>
+                        <Link className='cursor-pointer'>
+                            <span>Logout</span>
+                        </Link>
+                    </div>
+                    </>
+                ) : (
+                    <>
+                    <div className='p-2 cursor-pointer text-[14px]' >Enter</div>
+                    </>
+                )}
+            </div>
+        </nav>
+
+        {show && (
+            <div className='border-t border-white fixed top-14 right-0 bg-black h-full w-full z-99 justify-center items-center flex'>
+                <div className='flex flex-col top-0 absolute h-[400px] w-[250px] bg-black'>
+                    <Link className='text-white pt-10 pl-1'>
+                        <span>History</span>
+                    </Link>
+                    <hr className="border-t border-gray-500 my-4" />
+                    <Link className='text-white pl-1'>
+                        <span>Logout</span>
+                    </Link>
+                    <hr className="border-t border-gray-500 my-4" />
+                    <div className='justify-center items-center flex mt-4'>
+                        <X className='text-white' onClick={handleClick}/>
+                    </div>
+                    
+                </div>
+            </div>
+        )}
+    </>
   )
 }
 
